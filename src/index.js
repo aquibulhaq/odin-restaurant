ac71c8a3d3c07d18a66bb22ef3dc7d04a4bd1f92
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2025-04-07 01:09:59 +0600

    Wire up the navigation buttons

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@ import { getHomeContent } from './home/home.js';
 console.log('Hello, Odin Restaurant!');
 
 const contentDiv = document.querySelector('#content');
+
+const navDiv = document.querySelector('nav');
+navDiv.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (target.tagName.toLowerCase() !== 'button')
+    return;
+
+  contentDiv.textContent = '';
+
+  if (target.classList.contains('home-button'))
+    contentDiv.appendChild(getHomeContent());
+});
+
 contentDiv.appendChild(getHomeContent());
