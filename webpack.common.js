de4ebd7ba4f5d980666c02dbda469dd960c97710
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2025-05-17 00:04:45 +0600

    Split webpack config to development and production

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,12 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  devtool: 'eval-source-map',
-  devServer: {
-    watchFiles: ['./src/template.html'],
   },
   plugins: [
     new HTMLWebpackPlugin({
